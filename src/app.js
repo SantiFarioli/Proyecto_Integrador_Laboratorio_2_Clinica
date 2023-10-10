ed27b7a028ab5a944d7ad7efa20e7282c2beb629
Fermin2049 <fermin2049@gmail.com>
2023-10-10 12:54:53 -0300

    preprando las vistas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import { __dirname }from './utils.js';
+import { __dirname } from './utils.js';
 import * as path from 'path';
 
 const app = express();
@@ -10,10 +10,12 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.resolve(__dirname + '/views'));
 
+app.use('/', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.render('inicio');
+	res.render('inicio');
 });
 
 app.listen(port, () => {
-    console.log(`Escuchando en el puerto ${port}`);
+	console.log(`Escuchando en el puerto ${port}`);
 });
