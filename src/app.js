89ba2b06c8e81f3cc16285ccb76c43c29a089529
Fermin2049 <fermin2049@gmail.com>
2023-10-11 20:25:40 -0300

    rutas login

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import { __dirname } from './utils.js';
 import * as path from 'path';
-import {sequelize} from './database/database.js';
+import { sequelize } from './database/database.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,22 +14,27 @@ app.set('views', path.resolve(__dirname + '/views'));
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	res.render('inicio');
+	// Marca la variable isLoginPage como falsa cuando estás en la página de inicio.
+	res.render('inicio', { isLoginPage: false });
 });
 
+app.get('/login', (req, res) => {
+	// Marca la variable isLoginPage como verdadera cuando estás en la página de inicio de sesión.
+	res.render('loginPersonalLab', { isLoginPage: true });
+});
 
 import './models/paciente.js';
 import './models/orden_trabajo.js';
 
 async function main() {
-    try {
-        await sequelize.sync({force: true});
-        console.log('conexion establecida con exito');
+	try {
+		await sequelize.sync({ force: true });
+		console.log('conexion establecida con exito');
 		app.listen(port, () => {
 			console.log(`Escuchando en el puerto ${port}`);
 		});
-    } catch (error) {
-        console.log('No se puede establecer la conexion a la base de datos', error);
-    }
+	} catch (error) {
+		console.log('No se puede establecer la conexion a la base de datos', error);
+	}
 }
- main();
+main();
