c96eeaea17cf553916ce3ada449b385b97d9a667
Fermin2049 <fermin2049@gmail.com>
2023-10-11 03:06:00 -0300

    vista recepcionista

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,6 @@
+const toggler = document.querySelector('.btn');
+toggler.addEventListener('click', function () {
+	document.querySelector('#sidebar').classList.toggle('collapsed');
+});
+
 console.log('hola desde el js');
