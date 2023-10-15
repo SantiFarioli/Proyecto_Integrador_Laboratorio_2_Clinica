d082118b207412a9937edd5b618e21e8851c5a94
Santiago Farioli <santiago8773cba@gmail.com>
2023-10-15 09:51:59 -0300

    controllers

--- src/controllers/pacientes.controller.js
+++ src/controllers/pacientes.controller.js
@@ -1,29 +1,48 @@
 import {paciente} from '../models/paciente.js';
 
 export const getPacientes = async (req, res) => {
-    const pacientes = await paciente.findAll(); 
-    
-    res.json(pacientes);
+    try {
+        const pacientes = await paciente.findAll(); 
+        res.json(pacientes);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message            
+        })
+    }
 };
 
 export const createPaciente = async (req, res) => {
     const {nombre, apellido, dni, localidad, provincia, sexo, embarazo, fecha_nac, correo_electronico, telefono, obra_social, num_afiliado} = req.body;
 
-    const newPaciente = await paciente.create({
-        nombre,
-        apellido,
-        dni,
-        localidad,
-        provincia,
-        sexo,
-        embarazo,
-        fecha_nac,
-        correo_electronico,
-        telefono,
-        obra_social,
-        num_afiliado
-    });
-    
-    res.json(newPaciente);
+    try {
+        const newPaciente = await paciente.create({
+            nombre,
+            apellido,
+            dni,
+            localidad,
+            provincia,
+            sexo,
+            embarazo,
+            fecha_nac,
+            correo_electronico,
+            telefono,
+            obra_social,
+            num_afiliado
+        });
+        
+        res.json(newPaciente);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message            
+        })
+    }
 };
 
+export const updatePaciente = async (req, res) => {
+    
+}
+
+export const deletePaciente = async (req, res) => {
+    
+}
--- src/router/paciente.ruta.js
+++ src/router/paciente.ruta.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {getPacientes, createPaciente} from '../controllers/pacientes.controller.js'
+import {getPacientes, createPaciente, updatePaciente, deletePaciente} from '../controllers/pacientes.controller.js'
 
 const router = Router();
 
 router.get('/pacientes', getPacientes);
 router.post('/pacientes', createPaciente);
-router.put('/pacientes/:id')
-router.delete('/pacientes/:id')
+router.put('/pacientes/:id', updatePaciente)
+router.delete('/pacientes/:id', deletePaciente);
 router.get('/pacientes/:id')
 
 
