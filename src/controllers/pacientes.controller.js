136b31b95597d63fdbbf57ac02f8b204b0a4c72b
Fermin2049 <fermin2049@gmail.com>
2023-10-15 18:48:00 -0300

    carga de paciente

--- src/controllers/pacientes.controller.js
+++ src/controllers/pacientes.controller.js
@@ -1,48 +1,56 @@
-import {paciente} from '../models/paciente.js';
+import { paciente } from '../models/paciente.js';
 
 export const getPacientes = async (req, res) => {
-    try {
-        const pacientes = await paciente.findAll(); 
-        res.json(pacientes);
-
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message            
-        })
-    }
+	try {
+		const pacientes = await paciente.findAll();
+		res.json(pacientes);
+	} catch (error) {
+		return res.status(500).json({
+			message: error.message,
+		});
+	}
 };
 
 export const createPaciente = async (req, res) => {
-    const {nombre, apellido, dni, localidad, provincia, sexo, embarazo, fecha_nac, correo_electronico, telefono, obra_social, num_afiliado} = req.body;
+	const {
+		nombre,
+		apellido,
+		dni,
+		localidad,
+		provincia,
+		sexo,
+		embarazo,
+		fecha_nac,
+		correo_electronico,
+		telefono,
+		obra_social,
+		num_afiliado,
+	} = req.body;
 
-    try {
-        const newPaciente = await paciente.create({
-            nombre,
-            apellido,
-            dni,
-            localidad,
-            provincia,
-            sexo,
-            embarazo,
-            fecha_nac,
-            correo_electronico,
-            telefono,
-            obra_social,
-            num_afiliado
-        });
-        
-        res.json(newPaciente);
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message            
-        })
-    }
+	try {
+		const newPaciente = await paciente.create({
+			nombre,
+			apellido,
+			dni,
+			localidad,
+			provincia,
+			sexo,
+			embarazo,
+			fecha_nac,
+			correo_electronico,
+			telefono,
+			obra_social,
+			num_afiliado,
+		});
+
+		res.json(newPaciente);
+	} catch (error) {
+		return res.status(500).json({
+			message: error.message,
+		});
+	}
 };
 
-export const updatePaciente = async (req, res) => {
-    
-}
+export const updatePaciente = async (req, res) => {};
 
-export const deletePaciente = async (req, res) => {
-    
-}
+export const deletePaciente = async (req, res) => {};
