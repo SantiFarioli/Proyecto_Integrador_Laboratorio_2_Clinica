1739a1bf22a581eab1a9373b8550afe985937a33
Fermin2049 <fermin2049@gmail.com>
2023-10-22 20:31:53 -0300

    antes de empezar el pop del paciente actualizar

--- src/public/js/recepcionista.js
+++ src/public/js/recepcionista.js
@@ -1,9 +1,10 @@
 // Obtén los elementos del DOM que quieres manipular
 const formularioBusqueda = document.getElementById('formulario-busqueda');
 const formularioRegistro = document.getElementById('formulario-registro');
-let mostrarFormularioBusqueda = document.getElementById(
+const mostrarFormularioBusqueda = document.getElementById(
 	'mostrar-formulario-busqueda'
 );
+const tablaPacientes = document.getElementById('tablaDePacientes');
 const mostrarFormularioUpdate = document.getElementById(
 	'formulario-actualizar'
 );
@@ -26,6 +27,17 @@ const sexoSelect2 = document.getElementById('sexo2');
 const embarazoSi2 = document.getElementById('embarazo-si2');
 const embarazoNo2 = document.getElementById('embarazo-no2');
 
+function habilitarBotonSegunTabla() {
+	const filas = tablaPacientes.querySelectorAll('tbody tr');
+	mostrarFormularioBusqueda.disabled = filas.length > 0;
+}
+
+// Observador para detectar cambios en la tabla
+const observer = new MutationObserver(habilitarBotonSegunTabla);
+
+// Configura el observador para observar cambios en el contenido de la tabla
+const config = { childList: true, subtree: true };
+
 document.addEventListener('DOMContentLoaded', function () {
 	// Agrega un evento al enlace 'Registrar Paciente' en la barra lateral
 	formularioBusqueda.addEventListener('click', function (e) {
@@ -35,190 +47,69 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 
 	// Agrega un evento al botón 'Registrar Nuevo Paciente'
-	mostrarFormularioBusqueda.disabled = true;
+
 	mostrarFormularioBusqueda.addEventListener('click', function () {
 		formularioPaciente.classList.remove('d-none');
 		formularioRegistro.classList.add('d-none');
 	});
 
-	buscarPacienteButton.addEventListener('click', async function () {
-		// Realiza la búsqueda y muestra los resultados en la tabla
-		const criterio = criterioBusqueda.value;
-		const valor = valorBusqueda.value;
+	formularioBusqueda.addEventListener('click', async function (e) {
+		e.preventDefault();
 
 		try {
-			// Realiza una solicitud al servidor para buscar pacientes
-			const response = await fetch(
-				`/paciente?criterio=${criterio}&valor=${valor}`
-			);
+			const response = await fetch('/pacientes');
 			if (response.ok) {
 				const pacientes = await response.json();
-				console.log(pacientes);
-				if (pacientes.length > 0) {
-					// Si se encuentran pacientes, muestra los resultados en una tabla
-					const tablaResultados = `
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Apellido</th>
-                  <th>DNI</th>
-                  <th>Correo Electrónico</th>
-                  <th>Teléfono</th>
-                </tr>
-              </thead>
-              <tbody>
-                ${pacientes
-									.map(
-										(paciente) => `
-                    <tr>
-                      <td>${paciente.nombre}</td>
-                      <td>${paciente.apellido}</td>
-                      <td>${paciente.dni}</td>
-                      <td>${paciente.correo_electronico}</td>
-                      <td>${paciente.telefono}</td>
-                    </tr>
-                  `
-									)
-									.join('')}
-              </tbody>
-            </table>
-            <button id='actualizar-paciente' class='btn btn-primary mt-3'>Actualizar Paciente</button>
-          `;
-					resultados.innerHTML = tablaResultados;
+				renderPacientesTable(pacientes);
 
-					// Almacenar los campos del formulario
-					const nombreInput = document.getElementById('nombre2');
-					const apellidoInput = document.getElementById('apellido2');
-					const dniInput = document.getElementById('dni2');
-					const localidadInput = document.getElementById('localidad2');
-					const provinciaInput = document.getElementById('provincia2');
-					const fechaNacimientoInput = document.getElementById('fecha_nac2');
-					const correoElectronicoInput = document.getElementById(
-						'correo_electronico2'
-					);
-					const telefonoInput = document.getElementById('telefono2');
-					const obraSocialInput = document.getElementById('obra_social2');
-					const numAfiliadoInput = document.getElementById('num_afiliado2');
-					const sexoSelect2 = document.getElementById('sexo2');
-					const embarazoSi2 = document.getElementById('embarazo-si2');
-					const embarazoNo2 = document.getElementById('embarazo-no2');
-
-					const embarazoInput = embarazoSi2.checked ? true : false;
-
-					const actualizarPaciente = document.getElementById(
-						'actualizar-paciente'
-					);
-
-					actualizarPaciente.addEventListener('click', function () {
-						if (pacientes.length > 0) {
-							document
-								.getElementById('formulario-actualizar')
-								.classList.remove('d-none');
-							//mostrarFormularioUpdate.classList.remove('d-none');
-							// Supongamos que seleccionas el primer paciente de la lista
-							const pacienteSeleccionado = pacientes[0];
-							console.log(pacienteSeleccionado.idPaciente);
-							if (pacienteSeleccionado.embarazo === true) {
-								embarazoSi2.checked = true;
-								embarazoNo2.checked = false;
-							} else {
-								embarazoSi2.checked = false;
-								embarazoNo2.checked = true;
-							}
-
-							// Llenar el formulario con los datos del paciente seleccionado
-							nombreInput.value = pacienteSeleccionado.nombre;
-							apellidoInput.value = pacienteSeleccionado.apellido;
-							dniInput.value = pacienteSeleccionado.dni;
-							localidadInput.value = pacienteSeleccionado.localidad;
-							provinciaInput.value = pacienteSeleccionado.provincia;
-							fechaNacimientoInput.value = pacienteSeleccionado.fecha_nac;
-							correoElectronicoInput.value =
-								pacienteSeleccionado.correo_electronico;
-							telefonoInput.value = pacienteSeleccionado.telefono;
-							obraSocialInput.value = pacienteSeleccionado.obra_social;
-							numAfiliadoInput.value = pacienteSeleccionado.num_afiliado;
-							sexoSelect2.value = pacienteSeleccionado.sexo;
-							embarazoSi2.value = pacienteSeleccionado.embarazo;
-
-							habilitarEmbarazoPorSexo(sexoSelect2, embarazoSi2, embarazoNo2);
-
-							// Mostrar el formulario
-
-							// Manejar la actualización del paciente al hacer clic en "Enviar"
-							mostrarFormularioUpdate.addEventListener(
-								'submit',
-								async function (e) {
-									e.preventDefault();
-									const pacienteId = pacienteSeleccionado.idPaciente;
-
-									// Recopilar datos del formulario y enviarlos al servidor como una solicitud PUT
-									const datosPaciente = {
-										nombre: nombreInput.value,
-										apellido: apellidoInput.value,
-										dni: dniInput.value,
-										localidad: localidadInput.value,
-										provincia: provinciaInput.value,
-										fecha_nac: fechaNacimientoInput.value,
-										correo_electronico: correoElectronicoInput.value,
-										telefono: telefonoInput.value,
-										obra_social: obraSocialInput.value,
-										num_afiliado: numAfiliadoInput.value,
-										sexo: sexoInput.value,
-										embarazo: embarazoInput,
-									};
-
-									const response = await fetch(
-										`/pacientes/actualizar/${pacienteId}`,
-										{
-											method: 'PUT',
-											headers: {
-												'Content-Type': 'application/json',
-											},
-											body: JSON.stringify(datosPaciente),
-										}
-									);
-
-									if (response.ok) {
-										// Manejar la respuesta del servidor (por ejemplo, mostrar un mensaje de éxito)
-										Swal.fire({
-											icon: 'success',
-											title: 'Bioquimica Doña ADN',
-											text: 'Paciente Actualizado!',
-										}).then(() => {
-											// Redirigir al usuario a la pantalla principal
-											window.location.href = 'http://localhost:3000/';
-										});
-									} else {
-										// Manejar errores (por ejemplo, mostrar un mensaje de error)
-										Swal.fire({
-											icon: 'error',
-											title: 'Bioquimica Doña ADN',
-											text: 'Error al actualizar el paciente!',
-										});
-									}
-								}
-							);
-						}
-					});
-				} else {
-					// Si no se encuentran pacientes, muestra un mensaje
-					mostrarFormularioBusqueda.disabled = false;
-					Swal.fire({
-						icon: 'error',
-						title: 'Bioquimica Doña ADN',
-						text: 'Paciente No Encontrado!',
-					});
-				}
+				// Obtener las filas después de renderizar la tabla
 			} else {
-				console.error('Error en la solicitud');
+				console.error('Error al obtener los pacientes.');
 			}
 		} catch (error) {
-			console.error(error);
+			console.error('Error al obtener los pacientes:', error);
 		}
 	});
 });
+
+function renderPacientesTable(pacientes) {
+	const tableBody = document
+		.getElementById('tablaDePacientes')
+		.querySelector('tbody');
+
+	pacientes.forEach((paciente) => {
+		const newRow = tableBody.insertRow();
+		newRow.innerHTML = `
+			<td >${paciente.idPaciente}</td>
+			<td >${paciente.nombre}</td>
+			<td >${paciente.apellido}</td>
+			<td >${paciente.dni}</td>
+			<td >${paciente.localidad}</td>
+			<td >${paciente.provincia}</td>
+			<td >${paciente.sexo}</td>
+			<td >${paciente.embarazo ? 'Sí' : 'No'}</td>
+			<td >${paciente.fecha_nac}</td>
+			<td >${paciente.correo_electronico}</td>
+			<td >${paciente.telefono}</td>
+			<td >${paciente.obra_social}</td>
+			<td >${paciente.num_afiliado}</td>
+			<td>
+			<i class='fa-regular fa-file-lines'id="iconoOrden"></i>
+            <i class="fa-solid fa-pen" id="iconoPen"></i>
+
+			</td>
+		`;
+		tableBody.appendChild(newRow);
+	});
+
+	// Inicializa DataTable
+	$(document).ready(function () {
+		$('#tablaDePacientes').DataTable({
+			// Otras opciones de configuración
+			editable: true, // Habilitar la edición en línea
+		});
+	});
+}
 
 document.addEventListener('DOMContentLoaded', function () {
 	formularioRegistroPaciente2.addEventListener('submit', async function (e) {
@@ -241,7 +132,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		const embarazoInput = embarazoSi.checked ? true : false;
 
-		habilitarEmbarazoPorSexo(sexoSelect, embarazoSi, embarazoNo);
+		habilitarEmbarazoPorSexo(sexoInput, embarazoSi, embarazoNo);
 
 		// Realiza el registro del paciente y obtén la respuesta del servidor
 		const datosPaciente = {
@@ -293,7 +184,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function habilitarEmbarazoPorSexo(sexoSelect, embarazoSi, embarazoNo) {
 	if (sexoSelect) {
 		sexoSelect.addEventListener('change', function () {
-			if (sexoSelect.value === 'masculino') {
+			if (sexoSelect.value === 'Masculino') {
 				embarazoSi.disabled = true;
 				embarazoNo.disabled = true;
 				embarazoNo.checked = true; // Establecer en "No"
