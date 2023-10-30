aa0a82ea777892e6d7ceb865320e0462392e181e
Fermin2049 <fermin2049@gmail.com>
2023-10-30 11:05:34 -0300

    correcion commit anterior

--- src/models/examen.js
+++ src/models/examen.js
@@ -3,34 +3,36 @@ import { sequelize } from '../database/database.js';
 import { determinacion } from './determinacion.js';
 import { muestra } from './muestra.js';
 
-export const examen = sequelize.define('examenes', {
-	idExamen: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
+export const examen = sequelize.define(
+	'examenes',
+	{
+		idExamen: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		codigo: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		descripcion: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			timeStamp: true,
+		},
+		requisitosExamen: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		tiempoDeExamen: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
 	},
-	codigo: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	descripcion: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		timeStamp: true,
-	}, 
-	requisitosExamen: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	tiempoDeExamen: {
-		type: DataTypes.STRING,
-		allowNull: false,
+	{
+		timestamps: false,
 	}
-	
-}
-, {
-	timestamps: false
-});
+);
 
 examen.hasMany(determinacion, {
 	foreignKey: 'idExamen',
